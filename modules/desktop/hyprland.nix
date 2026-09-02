8d76bed2e5dea421ca3d058bc6b318ad3c81bc5a
Alfihri12 <alfihri221@gmail.com>
2026-09-03 05:37:24 +0700

    hyprland

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -1,7 +1,58 @@
-{ ... }:
+{ pkgs, ... }:
+
 {
+  programs.hyprland.enable = true;
 
-	# Hyprland
-	programs.hyprland.enable = true;
+  environment.systemPackages = with pkgs; [
+    # Hyprland
+    hyprland
+    hyprpolkitagent
+    hypridle
+    hyprlock
 
+    # Terminal
+    kitty
+
+    # XDG
+    xdg-user-dirs
+    xdg-desktop-portal
+    xdg-desktop-portal-hyprland
+
+    # Brightness
+    brightnessctl
+
+    # Keyring
+    gnome-keyring
+
+    # Fonts
+    noto-fonts
+    noto-fonts-emoji
+    noto-fonts-cjk
+
+    # File manager
+    thunar
+
+    # Wallpaper
+    swww
+    imagemagick
+
+    # Clipboard
+    cliphist
+    wl-clipboard
+
+    # Screenshot
+    grim
+    slurp
+
+    # Other
+    kdeconnect
+  ];
+
+  systemd.user.services.hyprpolkitagent = {
+    description = "Hyprpolkitagent";
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.hyprpolkitagent}/bin/hyprpolkitagent";
+    };
+  };
 }
