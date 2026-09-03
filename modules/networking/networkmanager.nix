33176f399112d0c88f0b5147f24ee3cc1f1c5daa
Alfihri12 <alfihri221@gmail.com>
2026-09-03 09:36:40 +0700

    hotspot langsung aktif

--- modules/networking/networkmanager.nix
+++ modules/networking/networkmanager.nix
@@ -1,19 +1,29 @@
 { ... }:
 {
-networking.hostName = "nixos";
-networking.networkmanager.enable = true;
+	networking.hostName = "nixos";
+	networking.networkmanager.enable = true;
 
-networking.nameservers = [
-"1.1.1.1"
-"8.8.8.8"
-];
+	networking.nameservers = [
+	"1.1.1.1"
+	"8.8.8.8"
+	];
 
-networking.firewall = {
-  enable = true;
-  # Izinkan port DHCP server dan DNS
-  allowedUDPPorts = [ 53 67 ];
-  allowedTCPPorts = [ 53 ];
-};
-networking.networkmanager.dns = "dnsmasq";
+	networking.firewall = {
+		enable = true;
+		# Izinkan port DHCP server dan DNS
+		allowedUDPPorts = [ 53 67 ];
+		allowedTCPPorts = [ 53 ];
+	};
+
+	networking.networkmanager.dns = "dnsmasq";
+
+	networking.networkmanager.ensureProfiles.profiles = {
+		Hotspot = {
+			connection = {
+				id = "Hotspot";
+				autoconnect = "true";
+			};
+		};
+	};
 
 }
